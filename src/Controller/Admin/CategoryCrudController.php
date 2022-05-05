3a114f120a686130ca26a48b427b02434b6c88e3
Abdel <marouanii.abdelhamid@gmail.com>
2022-05-05 14:21:58 +0200

    easy admin

---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use Doctrine\Common\Collections\ArrayCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+    public function __construct()
+    {
+        $this->category = new ArrayCollection();
+        $this->createdAt= new \DateTimeImmutable();
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+            ImageField::new('image')->setUploadDir("/public/images/category")->setBasePath("/images/category")->setRequired(false),
+        ];
+    }
+}
