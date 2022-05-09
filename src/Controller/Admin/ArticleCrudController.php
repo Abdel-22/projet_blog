a7cb258bb6923f2bb9193173408af2cba476669e
Abdel <marouanii.abdelhamid@gmail.com>
2022-05-09 16:52:42 +0200

    rectification pagination

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Common\Collections\ArrayCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -31,11 +35,11 @@ class ArticleCrudController extends AbstractCrudController
         ];
     }
 
-    
+
 
     public function __construct()
     {
         $this->category = new ArrayCollection();
-        $this->createdAt= new \DateTimeImmutable();
+        $this->createdAt = new \DateTimeImmutable();
     }
 }
