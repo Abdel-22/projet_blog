49ba6c341cb75991f01a27f74e1cf8cb0666711c
Abdel <marouanii.abdelhamid@gmail.com>
2022-05-09 11:48:17 +0200

    securité (login/logout)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
 {
@@ -24,19 +25,40 @@ class SecurityController extends AbstractController
     /**
      * @Route("/register", name="security_register")
      */
-    public function resgister(Request $request): Response
+    public function register(Request $request, UserPasswordHasherInterface  $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $password_hash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password_hash);
+
             $this->manager->persist($user);
             $this->manager->flush();
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute("security_login");
         }
+
         return $this->render('security/index.html.twig', [
             'controller_name' => "Inscription",
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig', [
+            
+        ]);
+    }
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
     }
 }
